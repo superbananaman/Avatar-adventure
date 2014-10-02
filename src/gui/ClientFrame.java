24104658675a47e66e5b1d6ca513bf4019514b45
Thiodexal <mahoneyd08@gmail.com>
2014-10-02 15:31:53 +1300

    Changed some console properties

--- src/gui/ClientFrame.java
+++ src/gui/ClientFrame.java
@@ -16,77 +16,80 @@ import Renderer.Tile;
 
 public class ClientFrame extends JFrame{
 
-	private JPanel contentPane;
-	private JPanel renderWindow;
+    private JPanel contentPane;
+    private JPanel renderWindow;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ClientFrame frame = new ClientFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ClientFrame frame = new ClientFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public ClientFrame() {
-		setTitle("Avatar Adventure! ");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 720);
+    /**
+     * Create the frame.
+     */
+    public ClientFrame() {
+        setTitle("Avatar Adventure! ");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 1000, 720);
 
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{800, 200, 0};
-		gbl_contentPane.rowHeights = new int[]{600, 120, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        GridBagLayout gbl_contentPane = new GridBagLayout();
+        gbl_contentPane.columnWidths = new int[]{800, 200, 0};
+        gbl_contentPane.rowHeights = new int[]{600, 120, 0};
+        gbl_contentPane.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+        gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+        contentPane.setLayout(gbl_contentPane);
 
-		//Render Window Placeholder
-		renderWindow = new RenderWindow("room1");
-		renderWindow.setBackground(Color.BLACK);
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		contentPane.add(renderWindow, gbc_panel);
-		renderWindow.setFocusable(true);
+        //Render Window Placeholder
+        renderWindow = new RenderWindow("room1");
+        renderWindow.setBackground(Color.BLACK);
+        GridBagConstraints gbc_panel = new GridBagConstraints();
+        gbc_panel.fill = GridBagConstraints.BOTH;
+        gbc_panel.insets = new Insets(0, 0, 5, 5);
+        gbc_panel.gridx = 0;
+        gbc_panel.gridy = 0;
+        contentPane.add(renderWindow, gbc_panel);
+        renderWindow.setFocusable(true);
 
-		//Inventory Place Holder
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_2.gridx = 1;
-		gbc_panel_2.gridy = 0;
-		contentPane.add(panel_2, gbc_panel_2);
+        //Inventory Place Holder
+        JPanel panel_2 = new JPanel();
+        panel_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+        gbc_panel_2.fill = GridBagConstraints.BOTH;
+        gbc_panel_2.insets = new Insets(0, 0, 5, 0);
+        gbc_panel_2.gridx = 1;
+        gbc_panel_2.gridy = 0;
+        contentPane.add(panel_2, gbc_panel_2);
 
-		//Consoleoutput placeholder
-		JLabel textArea = new JLabel();
-		GridBagConstraints gbc_textArea = new GridBagConstraints();
-		textArea.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		textArea.setBackground(Color.WHITE);
-		textArea.setOpaque(true);
-		gbc_textArea.insets = new Insets(0, 0, 0, 5);
-		gbc_textArea.fill = GridBagConstraints.BOTH;
-		gbc_textArea.gridx = 0;
-		gbc_textArea.gridy = 1;
-		contentPane.add(textArea, gbc_textArea);
+        //Consoleoutput placeholder
+        JLabel textArea = new JLabel();
+        GridBagConstraints gbc_textArea = new GridBagConstraints();
+        textArea.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        textArea.setBackground(Color.WHITE);
+        textArea.setOpaque(true);
+        textArea.setVerticalAlignment(SwingConstants.BOTTOM);
+        gbc_textArea.insets = new Insets(0, 0, 0, 5);
+        gbc_textArea.fill = GridBagConstraints.BOTH;
+        gbc_textArea.gridx = 0;
+        gbc_textArea.gridy = 1;
+        contentPane.add(textArea, gbc_textArea);
 
-		setVisible(true);
-	}
+        setVisible(true);
+
+        textArea.setText("I'm a console window!");
+    }
 
 }
