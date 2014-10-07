0c56f99d6d353a887ca1e837509b5b907191eac8
catsbumatt <matt.catsburg@gmail.com>
2014-10-08 12:34:05 +1300

    Testing to see if server is working properly

--- src/tests/Circle.java
+++ src/tests/Circle.java
@@ -6,15 +6,25 @@ import java.io.Serializable;
 
 public class Circle implements Serializable {
 	public int x;
-	public int y;	
+	public int y;
+	public final String uid;
 	public int width = 10;
 	public int height = 10;
 	public Color col;
 
-	public Circle(int x, int y, Color col){
+	public Circle(int x, int y, String uid, Color col){
 		this.x = x;
 		this.y = y;
+		this.uid = uid;
 		this.col = col;
+	}
+
+	public void moveHorizontal(int amt){
+		x = x + amt;
+	}
+
+	public void moveVertical(int amt){
+		y = y + amt;
 	}
 
 	public void draw(Graphics g){
--- src/tests/ClientTest.java
+++ src/tests/ClientTest.java
@@ -2,7 +2,8 @@ package tests;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.MouseEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -15,41 +16,50 @@ import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
 import ClientServer.*;
 
-public class ClientTest implements MouseListener {
+public class ClientTest implements KeyListener {
 
 	ObjectInputStream in;
-	ObjectOutputStream out;
+	static ObjectOutputStream out;
 	JFrame frame = new JFrame("Circles");
 	JPanel canvas;
 	List<Circle> circles = new ArrayList<Circle>();
-	//Circle circle;
+	Circle current;
 
-	public ClientTest() {
-		// Layout GUI
-		canvas = new JPanel() {
-			@Override
-			public void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				// Must add or else it will lag
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				drawCircle(g);
-				// System.out.println("hey");
-			}
-		};
-		canvas.setSize(500, 500);
-		frame.add(canvas);
-		frame.setSize(500, 500);
-		frame.setResizable(false);
-		canvas.addMouseListener(this);
-
+	public ClientTest(Circle c) {
+		current = c;
+		//startClient();
 	}
 
+	public void startClient(){
+		// Layout GUI
+
+				canvas = new JPanel() {
+					@Override
+					public void paintComponent(Graphics g) {
+						super.paintComponent(g);
+						// Must add or else it will lag
+						try {
+							Thread.sleep(10);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						drawCircle(g);
+						// System.out.println("hey");
+					}
+				};
+				canvas.setSize(500, 500);
+				frame.add(canvas);
+				frame.setSize(500, 500);
+				frame.setResizable(false);
+				frame.addKeyListener(this);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+
+
+	}
 	/**
 	 * Prompt for and return the address of the server.
 	 */
@@ -61,16 +71,25 @@ public class ClientTest implements MouseListener {
 
 	private void drawCircle(Graphics g) {
 		synchronized (circles) {
-			g.setColor(Color.BLACK);
+			// g.setColor(Color.RED);
 			for (Circle circ : circles) {
 				circ.draw(g);
 			}
 			// System.out.println(circles.size());
-//		if (circle != null){
-//		circle.draw(g);
+			// if (circle != null){
+			// circle.draw(g);
 			canvas.repaint();
-//	}
+			// }
 		}
+	}
+
+	public Circle circle(String uid) {
+		for (Circle c : circles) {
+			if (c.uid.equals(uid)) {
+				return c;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -87,18 +106,88 @@ public class ClientTest implements MouseListener {
 		out = new ObjectOutputStream(socket.getOutputStream());
 		in = new ObjectInputStream(socket.getInputStream());
 
+		// send the Clients uid
+		System.out.println("(Client)about to write uid");
+		out.writeObject((Circle)current);
+		System.out.println("(Client)Written uid");
 		// Process all messages from server, according to the protocol.
-		while (true) {
 
-			// System.out.println("client stuck in run");
-			// System.out.println("about to read line");
-			Circle c = (Circle) in.readObject();
-			System.out.println(c.x + " " + c.y);
-			circles.add(c);
-			//circle = c;
+		boolean canstart = false;
+		//wait for response for server to
+		while (!canstart){
+			System.out.println("in canstart");
+			Object o = in.readObject();
+			System.out.println(o.toString());
+			if (o instanceof Circle){
+				System.out.println("CIRCLE ADDED");
+				Circle c = (Circle) o; 
+				System.out.println(c.x + " " + c.y);
+				circles.add(c);
+			}
+			if (o instanceof String){
+				System.out.println("breaking loop");
+				break;
+			}
+		}
+		startClient();
+		String uid = "";
+		while (true) {
+			Object o = in.readObject();
+//			if (o instanceof Circle){
+//				System.out.println("Circle has been added");
+//				Circle c = (Circle) o;
+//				System.out.println(c.x + " " + c.y);
+//				circles.add(c);
+//			}
+//			if (o instanceof Integer){
+//				Integer i = (Integer)o;
+//				if (i.equals(1)){
+//					circle(uid).moveVertical(-10);
+//				}
+//				else if (i.equals(2)){
+//					circle(uid).moveVertical(10);
+//				}
+//				else if (i.equals(3)){
+//					circle(uid).moveHorizontal(-10);
+//				}
+//				else if (i.equals(4)){
+//					circle(uid).moveHorizontal(10);
+//				}
+//				// the uid
+//				else{
+//					System.out.println("Setting uid");
+//					uid = i;
+//				}
+			if (o instanceof UIDObjectPair){
+				UIDObjectPair i = (UIDObjectPair)o;
+				uid = i.getUID();
+				int move = (Integer)i.getObject();
+				if (move == 1){
+					circle(uid).moveVertical(-10);
+				}
+				else if (move == 2){
+					circle(uid).moveVertical(10);
+				}
+				else if (move == 3){
+					circle(uid).moveHorizontal(-10);
+				}
+				else if (move == 4){
+					circle(uid).moveHorizontal(10);
+				}
+//				// the uid
+//				else{
+//					System.out.println("Setting uid");
+//					uid = i;
+//				}
+			}
+
+
+			// circle = c;
 
 		}
 	}
+
+
 
 	/**
 	 * Runs the client as an application with a closeable frame.
@@ -113,35 +202,66 @@ public class ClientTest implements MouseListener {
 				options, // the titles of buttons
 				options[0]); // default button title
 		if (n == 0){
-			Server s = new Server(9003);
+			Server s = new Server(2, 9003);
 		}
 		else if(n == 1){
-			ClientTest client = new ClientTest();
-			client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			client.frame.setVisible(true);
+			//ClientTest client = new ClientTest(new Circle(200, 200, "a", Color.RED));
+			ClientTest client = new ClientTest(new Circle(10, 10, "b", Color.BLUE));
+			//ClientTest client = new ClientTest(new Circle(300, 200, "c", Color.CYAN));
+//			client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//			client.frame.setVisible(true);
 			client.run();
+		//	Slave s = new Slave(new Socket(getServerAddress(), 9003));
 		}
+		
+		
+		
+//		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		client.frame.setVisible(true);
+//		client.run();
 	}
 
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		// circles.add(new Circle(e.getX(), e.getY()));
+	// @Override
+	// public void mousePressed(java.awt.event.MouseEvent e) {
+	// // TODO Auto-generated method stub
+	// // circles.add(new Circle(e.getX(), e.getY()));
+	// try {
+	// System.out.println("writing object");
+	// out.writeObject(new Circle(e.getX(), e.getY(), Color.RED));
+	// } catch (IOException e1) {
+	//
+	// e1.printStackTrace();
+	// }
+	// }
+
+	
+	public void keyTyped(KeyEvent e) {
+	}
+
+	
+	public void keyPressed(KeyEvent e) {
 		try {
-			System.out.println("writing object");
-			out.writeObject(new Circle(e.getX(), e.getY(), Color.RED));
-		} catch (IOException e1) {
-
-			e1.printStackTrace();
+			//System.out.println(circles.size());
+			int code = e.getKeyCode();
+			//System.out.println("sending int");
+			if (code == KeyEvent.VK_UP) {
+				out.writeObject(new UIDObjectPair(current.uid, new Integer(1)));				
+			}
+			else if(code == KeyEvent.VK_DOWN){
+				out.writeObject(new UIDObjectPair(current.uid, new Integer(2)));
+			}
+			else if(code == KeyEvent.VK_LEFT){
+				out.writeObject(new UIDObjectPair(current.uid, new Integer(3)));
+			}
+			else if(code == KeyEvent.VK_RIGHT){
+				out.writeObject(new UIDObjectPair(current.uid, new Integer(4)));
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
 	}
 
-
-
-	public void mouseClicked(MouseEvent e) {}
-
-	public void mouseReleased(MouseEvent e) {}
-
-	public void mouseEntered(MouseEvent e) {}
-
-	public void mouseExited(MouseEvent e) {}
+	
+	public void keyReleased(KeyEvent e) {
+	}
 }
