b7e17e09424ecba51b07dbbf3e89e3187407792f
Brendan Smith <smithbren3@fuel.ecs.vuw.ac.nz>
2014-10-09 16:32:20 +1300

    Added JDOM to the build path, and started the xml writer and parser

--- src/Database/XMLParser.java
+++ src/Database/XMLParser.java
@@ -1,5 +1,26 @@
 package Database;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
 public class XMLParser {
 
+	private String fileName;
+
+	public XMLParser(String filename){
+		fileName = filename;
+	}
+
+	public void readFromXML(){}
+
+
+	public Document useSAXParser(String filename) throws JDOMException, IOException {
+	// creating JDOM document from SAX parser
+	SAXBuilder saxBuilder = new SAXBuilder();
+	return saxBuilder.build(new File(filename));
+	}
 }
--- src/Database/XMLWriter.java
+++ src/Database/XMLWriter.java
@@ -1,5 +1,18 @@
 package Database;
 
-public class XMLReader {
+import org.jdom2.Document;
+import org.jdom2.Element;
 
+public class XMLWriter {
+
+	private String fileName;
+
+	public XMLWriter(String filename){
+		fileName = filename;
+	}
+
+	public void writeToXML(){
+		Element game = new Element("game");
+		Document doc = new Document(game);
+	}
 }
